ce86311690a62b7bf8efbcd13bb97415506aff5b
Ali POLAT <84542348+alipolat-js@users.noreply.github.com>
2022-03-01 23:28:21 +0300

    Update index.js

--- environment/index.js
+++ environment/index.js
@@ -1,3 +1,3 @@
-export const URL = process.env.NODE_ENV === "production" ? 'https://nextjsblogappwithmarkdown.alipolat.tech' : 'http://localhost:3000'
+export const URL = process.env.NODE_ENV === "production" ? 'https://nextjs-blog-app-with-md.alipolat.tech' : 'http://localhost:3000'
 
 // if you are going to production change the URL above
