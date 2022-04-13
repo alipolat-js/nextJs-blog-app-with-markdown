687fa0e3b55ff9994713ea1a1a05b5c2030f373e
Ali POLAT <84542348+alipolat-js@users.noreply.github.com>
2022-04-13 18:02:24 +0300

    Update admin.js

--- src/admin.js
+++ src/admin.js
@@ -2,7 +2,7 @@ const admin = {
   fullName: "Ali POLAT",
   about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, ducimus blanditiis, minima eum autem expedita repellendus delectus rerum fuga dicta dolor voluptate ea explicabo dolorum molestias suscipit ut placeat labore dolores voluptas tempora tenetur consectetur modi. Quidem hic ipsa iusto ea et accusamus provident quas recusandae dolorum voluptate laborum mollitia cum blanditiis aspernatur, doloribus officiis numquam velit ullam nam delectus, voluptatum eligendi impedit nihil temporibus? Ipsam quibusdam expedita deserunt, fuga maxime corrupti optio asperiores ipsa.",
   job: "Full Stack Web Developer",
-  linkedinAccount: "https://www.instagram.com/__alipolat",
+  linkedinAccount: "https://www.linkedin.com/in/-alipolat/",
   twitterAccount: "https://twitter.com/_alipolat",
   githubAccount: "https://github.com/alipolat-js",
 };
